5670bcf73997184648a11ac6a660f1e5455fe4aa
Ekoban <robinet.alexandre@gmail.com>
2018-11-28 16:19:54 +0100

    completed'

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
 const express = require('express');
-const app = express();
+const hbs = require('hbs');
+const fs = require('fs');
 
-app.use('/htmltest', express.static('public'));
-app.get('/htmltest', function (req, res) {
-    res.send(window.console.log('works ?'))
-})
+const port = process.env.PORT || 3000;
+var app = express();
 
-app.get('/bad', function (req, res) {
-    res.send({
-        errorMessage: 'Wrong path - go home'
-    })
-})
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
-app.listen(7998)
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
